babc70d5a56a91834389b823d35f1bec1716174f
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-03-01 10:22:11 +0200

    BottomModal: indexed

--- src/src/components/index.tsx
+++ src/src/components/index.tsx
@@ -1,7 +1,8 @@
 import AppBar from './AppBar/AppBar';
+import BottomModal from './BottomModal/BottomModal';
 import CircleIcon from './CircleIcon/CircleIcon';
 import Icon from './Icon/Icon';
 import Text from './Text/Text';
 import TouchFiller from './TouchFiller/TouchFiller';
 
-export {AppBar, CircleIcon, Icon, Text, TouchFiller};
+export {AppBar, BottomModal, CircleIcon, Icon, Text, TouchFiller};
--- src/src/screens/Components2/Components2.tsx
+++ src/src/screens/Components2/Components2.tsx
@@ -2,11 +2,41 @@ import {MainLayout} from '@hoc';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {Text, View} from 'react-native';
+import {BottomModal} from '@components';
+import {useAppTheme} from '@theme';
+
+const HR = ({height = 2}: {height?: number}) => {
+  return (
+    <View
+      className="bg-black my-3"
+      style={{
+        height,
+      }}
+    />
+  );
+};
+
+const Header = ({title = 'Title'}: {title: string}) => {
+  const colors = useAppTheme().colors;
+
+  return (
+    <View>
+      <HR height={4} />
+      <Text
+        className="font-bold text-[25px]"
+        style={{color: colors.normalText}}>
+        {title}
+      </Text>
+      <HR height={1} />
+    </View>
+  );
+};
 
 const Components2 = () => {
   return (
     <View>
-      <Text>Components2</Text>
+      <Header title="Text" />
+      <BottomModal />
     </View>
   );
 };
