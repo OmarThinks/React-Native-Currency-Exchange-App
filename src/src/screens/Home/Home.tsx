3506f741d3884a79f8257cafacfd87db0e53cc3d
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-03-01 16:37:28 +0200

    Chart: seperated in a component

--- src/src/screens/Home/Home.tsx
+++ src/src/screens/Home/Home.tsx
@@ -1,34 +1,24 @@
-import {Text} from '@components';
 import {MainLayout} from '@hoc';
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
-import {useTranslation} from 'react-i18next';
-import {View, Image} from 'react-native';
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-} from 'react-native-chart-kit';
-import {useWindowDimensions} from 'react-native';
 import {useAppTheme} from '@theme';
-
-import axios from 'axios';
-import {Button} from 'react-native-paper';
-
-type UserdataType = {
-  avatar: string;
-  email: string;
-  first_name: string;
-  id: number;
-  last_name: string;
-};
+import React, {memo, useCallback} from 'react';
+import {useTranslation} from 'react-i18next';
+import {View, useWindowDimensions} from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
 
 const Home = () => {
   const {t} = useTranslation();
   // console.log(i18n.language);
 
+  const colors = useAppTheme().colors;
+
+  return (
+    <View>
+      <Chart />
+    </View>
+  );
+};
+
+const Chart = memo(() => {
   const {width} = useWindowDimensions();
 
   const theme = useAppTheme();
@@ -51,56 +41,52 @@ const Home = () => {
     },
     [theme.dark],
   );
-
   return (
-    <View>
-      <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={{
-          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-          datasets: [
-            {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
-            },
-          ],
-        }}
-        width={width - 30} // from react-native
-        height={220}
-        yAxisLabel="$"
-        yAxisSuffix="k"
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={{
-          backgroundColor: 'yellow',
-          backgroundGradientFrom: colors.chartBgColor,
-          backgroundGradientTo: colors.chartBgColor,
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: chartColor,
-          labelColor: labelColor, //`rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
+    <LineChart
+      data={{
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+          {
+            data: [
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+            ],
           },
-          propsForDots: {
-            r: '6',
-            strokeWidth: '2',
-            stroke: '#ffa726',
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
+        ],
+      }}
+      width={width - 30} // from react-native
+      height={220}
+      yAxisLabel="$"
+      yAxisSuffix="k"
+      yAxisInterval={1} // optional, defaults to 1
+      chartConfig={{
+        backgroundColor: 'yellow',
+        backgroundGradientFrom: colors.chartBgColor,
+        backgroundGradientTo: colors.chartBgColor,
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: chartColor,
+        labelColor: labelColor,
+        style: {
           borderRadius: 16,
-        }}
-      />
-    </View>
+        },
+        propsForDots: {
+          r: '6',
+          strokeWidth: '2',
+          stroke: colors.normalText,
+        },
+      }}
+      bezier
+      style={{
+        marginVertical: 8,
+        borderRadius: 16,
+      }}
+    />
   );
-};
+});
 
 export default () => {
   const {t} = useTranslation();
